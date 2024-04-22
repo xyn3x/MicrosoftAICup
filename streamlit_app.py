f9f0b28945afb086da774025770fb8e7c79a94f6
xyn3x <tenjjiin@gmail.com>
2024-04-22 10:53:04 +0500

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
-with open('.devcontainer/template/index.html', 'r') as f:
-    html_string = f.read()
-print(html_string)
+from flask import Flask, request, render_template
+app = Flask(__name__, template_folder='template')
+
+def main():
+    if request.method == 'GET':
+        return (render_template('index.html'))
+    
+if __name__ == '__main__':
+    app.run()
