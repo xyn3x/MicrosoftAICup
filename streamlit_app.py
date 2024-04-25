16cff0564f7dccd20fd6fde7b71406cf61e80359
xyn3x <tenjjiin@gmail.com>
2024-04-25 12:54:20 +0500

    fixed some bugs

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,12 @@
 import flask as fl
 app = fl.Flask(__name__, template_folder='templates')
 
-@app.route('/')
+@app.route('/', methods = ['GET', 'POST'])
 def main():
     if fl.request.method == 'GET':
         return (fl.render_template('main.html'))
+    if fl.request.method == 'POST':
+        age = fl.request.form.get('age')
+        return (fl.render_template('result.html', age=age))
 if __name__ == '__main__':
     app.run()
